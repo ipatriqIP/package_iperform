a58eaf3e49af296026b0efb168f1d91899197f1e
Patrick ILUNGA <patriqyoo@gmail.com>
2023-11-14 13:15:34 +0100

    Ajout de la fonction qtd

---
+++ R/qtd.R
@@ -0,0 +1,46 @@
+#' @importFrom lubridate wday mday yday week month quarter year leap_year is.Date
+NULL
+
+#' @importFrom matrixStats rowMins rowMaxs
+NULL
+
+#' Calcul de la valeur qtd d'une serie.
+#'
+#' Cette fonction permet de trouver la valeur d'une série à une date voulue par l'utilisateur, le d-day.
+#'
+#' @param data Un objet de type data.frame contenant au minimum la serie numerique et un vecteur de type date.
+#' @param date La date que l'on souhaite connaitre la valeur de la serie.
+#' @param q Une valeur de type entier qui permet de décaler le trimestre par rapport à la date introduite. Sa valeur par defaut est `0`.
+#' @param x Un vecteur de type numeric contenant les observations du phénomène à etudier.
+#' @param unite Un valeur de type numeric, definissant l'ordre d'echelle d'affisage du resultat de la fonction. Sa valeur par defaut est `1`.
+#' @param decimal Un objet de type numeric permettant de préciser le nombre d'entier à afficher après la virgule. Sa valeur par defaut est `0`.
+#'
+#' @return Un vecteur de longieur 1.
+#'
+#' @encoding UTF-8
+#' @export
+#'
+#' @examples
+#' x <- rnorm(222, mean = 50, sd = 6.3)
+#' date <- seq.Date(as.Date("2023-01-01"), by = "day", len = 222)
+#' df <- data.frame(date, x)
+#' qtd(df, date = "2023-07-06", x = "x")
+#'
+qtd <- function(data,
+                date,
+                q = 0,
+                x,
+                unite = 1,
+                decimal = 0) {
+
+  stopifnot(is.data.frame(data), is.Date(as.Date(date)), is.numeric(q), is.numeric(data[, x]))
+  stopifnot("date" %in% colnames(data), x %in% colnames(data))
+
+  annee <- year(date)
+  trimestre <- quarter(date) + q
+
+  valeur <- data[data[, "date"] == as.Date(date) + q, x]
+  valeur <- round(valeur/unite, decimal)
+
+  return(valeur)
+  }
---
+++ tests/testthat/test-qtd.R
@@ -0,0 +1,4 @@
+test_that("qtd works", {
+  expect_equal(2 * 2, 4)
+
+  })
